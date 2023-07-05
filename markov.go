c1c59a657bbb228e10ff0085ddf03501a1afa085
Christopher De Vries <devries@idolstarastronomer.com>
2023-07-05 13:12:43 -0400

    fix some issues with markov chain

--- markov.go
+++ markov.go
@@ -3,27 +3,37 @@ package nlm
 // MarkovBuilder is a map of strings pointing to counts of letters
 // that follow each string. This is used while building a Markov
 // chain structure
-type MarkovBuilder map[string]map[rune]int
+type MarkovBuilder struct {
+	Counts   map[string]map[rune]int
+	Fragment []rune
+}
 
 const EndOfParagraph = '\u001d'
 const EndOfDocument = '\u0003'
 
-func NewMarkovBuilder() MarkovBuilder {
-	return make(map[string]map[rune]int)
+func NewMarkovBuilder() *MarkovBuilder {
+	return &MarkovBuilder{make(map[string]map[rune]int), []rune{}}
 }
 
 // AddText adds text to the Markov Builder, creating a table that will work
 // with a chain length of size. It also includes a
 // defined endCharacter that indicates the end of the passage.
-func (b MarkovBuilder) AddText(text string, size int, endCharacter rune) {
-	letters := []rune(text)
+func (b *MarkovBuilder) AddText(text string, size int, endCharacter rune) {
+	letters := make([]rune, len(b.Fragment))
+	copy(letters, b.Fragment)
+	letters = append(letters, []rune(text)...)
 
-	for i := 0; i < len(letters)-size; i++ {
-		substring := string(letters[i : i+size])
-		v := b[substring]
+	for i := 0; i <= len(letters)-size; i++ {
+		var substring string
+		if i+size == len(letters) {
+			substring = string(letters[i:])
+		} else {
+			substring = string(letters[i : i+size])
+		}
+		v := b.Counts[substring]
 		if v == nil {
 			v = make(map[rune]int)
-			b[substring] = v
+			b.Counts[substring] = v
 		}
 
 		if i+size >= len(letters) {
@@ -32,14 +42,17 @@ func (b MarkovBuilder) AddText(text string, size int, endCharacter rune) {
 			v[letters[i+size]] += 1
 		}
 	}
+	b.Fragment = make([]rune, size)
+	copy(b.Fragment, letters[len(letters)-size+1:])
+	b.Fragment[size-1] = endCharacter
 }
 
 type MarkovSource map[string](*CountWeightedList[rune])
 
-func (b MarkovBuilder) ConvertToSource() MarkovSource {
+func (b *MarkovBuilder) ConvertToSource() MarkovSource {
 	ret := make(map[string](*CountWeightedList[rune]))
 
-	for k, v := range b {
+	for k, v := range b.Counts {
 		ret[k] = NewCountWeightedList(v)
 	}
 
